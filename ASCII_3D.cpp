1796f50378e4d25c208569d9eaa8758f5633992b
OSEB49 <84204929+OSEB49@users.noreply.github.com>
2022-08-25 18:00:41 +0100

    Update ASCII_3D.cpp
    Basics of wall and player position.

--- ASCII_3D.cpp
+++ ASCII_3D.cpp
@@ -1,10 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
+#include<chrono>
 using namespace std;
 #include <Windows.h>
 
 int ScreenWidth = 120;
 int ScreenHeight = 40;
-
+//player's x,y and angle position
 float FPX = 8.0f;
 float FPY = 8.0f;
 float FPA = 0.0f;
@@ -15,34 +16,51 @@ float ViewRange = 3.14159 / 4.0;
 float MaxDepth = 16.0f;
 
 int main() {
-	
-	wstring map;
-	map += L"||||||||||||||||||||";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"|..................|";
-	map += L"||||||||||||||||||||";
 
-	wchar_t * screen = new wchar_t[ScreenWidth*ScreenHeight];
+
+	wchar_t* screen = new wchar_t[ScreenWidth * ScreenHeight];
 	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytesWritten = 0;
 
-	screen[ScreenWidth * ScreenHeight - 1] = '\0';
-	WriteConsoleOutputCharacter(hConsole, screen, ScreenWidth * ScreenHeight, { 0,0 }, &dwBytesWritten);
+	//map line by line
+	wstring map;
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"################";
 
-	while(1)
+	auto tp1 = chrono::system_clock::now();
+	auto tp2 = chrono::system_clock::now();
+
+
+
+	
+
+	while (1) {
+		tp2 = chrono::system_clock::now();
+		chrono::duration<float> elapsedTime = tp2 - tp1;
+		tp1 = tp2;
+		float ElapsedTime = elapsedTime.count();
+
+
+
+		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
+			FPA -= (0.1f) * ElapsedTime;
+		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
+			FPA += (0.1f) + ElapsedTime;
 
 		for (int x = 0; x < ScreenWidth; x++)
 		{
@@ -59,27 +77,28 @@ int main() {
 
 				int TestX = (int)(FPX + EyeX * WallDistance);
 				int TestY = (int)(FPY + EyeY * WallDistance);
-		
+
 				if (TestX < 0 || TestX >= MapWidth || TestY < 0 || TestY >= MapHeight)
 				{
 					HitTheWall = true;
 					WallDistance = MaxDepth;
-			}
+				}
 				else {
-					if (map[TestY * MapWidth + TestX] == '#') {
+					if (map.c_str()[TestY * MapWidth + TestX] == '#') {
 						HitTheWall = true;
 					}
 				}
-			
+
 			}
-			int Celling = (float)(ScreenHeight / 2.0) - ScreenHeight / (float)WallDistance;
+			//calculation of celling and floor
+			int Celling = (float)(ScreenHeight / 2.0) - ScreenHeight / ((float)WallDistance);
 			int Floor = ScreenHeight - Celling;
 
-			for (int y=0; y < ScreenHeight; y++) {
+			for (int y = 0; y < ScreenHeight; y++) {
 				if (y < Celling)
 					screen[y * ScreenWidth + x] = ' ';
-				else if(y > Celling && y <= Floor)
-					screen[y*ScreenWidth + x] = '#';
+				else if (y > Celling && y <= Floor)
+					screen[y * ScreenWidth + x] = '#';
 				else
 					screen[y * ScreenWidth + x] = ' ';
 			}
@@ -87,12 +106,9 @@ int main() {
 
 		}
 
-
-
-
-
-
-
+		screen[ScreenWidth * ScreenHeight - 1] = '\0';
+		WriteConsoleOutputCharacter(hConsole, screen, ScreenWidth * ScreenHeight, { 0,0 }, &dwBytesWritten);
+	}
 
 
 	return 0;
